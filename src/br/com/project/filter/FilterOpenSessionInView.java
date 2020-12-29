a65de8eeb6476917bb27a683eae88fab2b7f0eaf
Douglas <douglas@mouseweb.com.br>
2020-12-28 23:58:00 -0300

    Criado o Filter FilterOpenSessionInView

---
+++ src/br/com/project/filter/FilterOpenSessionInView.java
@@ -0,0 +1,122 @@
+package br.com.project.filter;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+import br.com.framework.hibernate.session.HibernateUtil;
+import br.com.framework.utils.UtilFramework;
+import br.com.project.listener.ContextLoaderListenerCaixakiUtils;
+import br.com.project.model.classes.Entidade;
+
+/**
+ * Responsavel por iniciar a sessão/transaction do hibernate, iterceptar as
+ * requisições/response, commit e rollback.
+ * 
+ * @author Douglas
+ * 
+ */
+@SuppressWarnings("unused")
+@WebFilter(filterName = "conexaoFilter")
+public class FilterOpenSessionInView extends DelegatingFilterProxy implements
+		Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private static SessionFactory sf;
+
+	// executa apenas uma vez
+	// executado  quando a aplicação está sendo iniciada
+	@Override
+	public void initFilterBean() throws ServletException {
+		sf = HibernateUtil.getSessionFactory();
+	}
+
+	@Override
+	public void doFilter(ServletRequest servletRequest,
+			ServletResponse servletResponse, FilterChain chain)
+			throws IOException, ServletException {
+
+		// JDBC Spring
+		BasicDataSource springDataSource = (BasicDataSource) ContextLoaderListenerCaixakiUtils.getBean("springDataSource");
+		DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+		PlatformTransactionManager transactionManager = new DataSourceTransactionManager(springDataSource);
+		TransactionStatus status = transactionManager.getTransaction(def);
+
+		try {
+
+			servletRequest.setCharacterEncoding("UTF-8"); // Define codificação 
+
+			// captura usuário que faz a operação
+			HttpServletRequest request = (HttpServletRequest) servletRequest;
+			HttpServletResponse response = (HttpServletResponse) servletResponse;
+			HttpSession sessao = request.getSession();
+			Entidade userLogadoSessao = (Entidade) sessao.getAttribute("userLogadoSessao");
+
+			if (userLogadoSessao != null) {
+				UtilFramework.getThreadLocal().set(userLogadoSessao.getEnt_codigo());
+			}
+
+			sf.getCurrentSession().beginTransaction();
+			
+			// antes de executar ação (Request)
+			chain.doFilter(servletRequest, servletResponse); // executa nossa ação no servidor 
+			// após de executar ação (Resposta)
+
+			// JDBC Spring - commit
+			transactionManager.commit(status);
+
+			// transação do hibernerte - executa os sql (flush) e commita
+			if (sf.getCurrentSession().getTransaction().isActive()) {
+				sf.getCurrentSession().flush();
+				sf.getCurrentSession().getTransaction().commit();
+			}
+
+			if (sf.getCurrentSession().isOpen()) {
+				sf.getCurrentSession().close();
+			}
+
+			servletResponse.setCharacterEncoding("UTF-8");
+			servletResponse.setContentType("text/html; charset=UTF-8");
+
+		} catch (Exception e) {
+
+			transactionManager.rollback(status);
+
+			e.printStackTrace();
+
+			if (sf.getCurrentSession().getTransaction().isActive()) {
+				sf.getCurrentSession().getTransaction().rollback();
+			}
+			if (sf.getCurrentSession().isOpen()) {
+				sf.getCurrentSession().close();
+			}
+		} finally {
+			if (sf.getCurrentSession().isOpen()) {
+				if (sf.getCurrentSession().beginTransaction().isActive()) {
+					sf.getCurrentSession().flush();
+					sf.getCurrentSession().clear();
+				}
+				if (sf.getCurrentSession().isOpen()) {
+					sf.getCurrentSession().close();
+				}
+			}
+
+		}
+	}
+}
